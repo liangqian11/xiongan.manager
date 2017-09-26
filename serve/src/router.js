9cf7b488344c8be0409d42e5c1a9aa2dced5d04e
liuyaqiandahaoren <1101200921@qq.com>
2017-09-26 14:51:55 +0800

    last

--- serve/src/router.js
+++ serve/src/router.js
@@ -5,8 +5,6 @@ const cultivate = require('./router/xiongan/cultivate')
 const examine = require('./router/xiongan/examine')
 const home = require('./router/xiongan/home')
 const live= require('./router/xiongan/live')
-//---------------------------------------------------------------------------- 测试上传
-// const upload = require('./router/test/upload')
 //---------------------------------------------------------------------------- 合并子路由
 const xiongan = _.merge(cultivate, examine, home, live)
 //---------------------------------------------------------------------------- 全并所有路由
