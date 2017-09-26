1fb1abc823373f61a863fc1cc4429176cb8164ea
liuyaqiandahaoren <1101200921@qq.com>
2017-09-26 13:53:15 +0800

    la

--- front/src/config/config/dev.js
+++ front/src/config/config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: {
-    upload: 'http://upload.mod.hesq.com.cn',
-    restful: 'http://192.168.0.98:5120/rest',
+    upload: 'http://192.168.0.100:3000',
+    restful: 'http://192.168.0.100:3000',
   }
 }
--- serve/dist/router.js
+++ serve/dist/router.js
@@ -1,16 +1,3 @@
 'use strict';
 
 var _ = require('lodash');
-
-var article = require('./router/xiongan/article');
-var company = require('./router/xiongan/company');
-var culture = require('./router/xiongan/culture');
-var login = require('./router/xiongan/login');
-var policy = require('./router/xiongan/policy');
-var wanted = require('./router/xiongan/wanted');
-
-var xiongan = _.merge(article, company, culture, login, policy, wanted);
-
-var router = _.merge(xiongan);
-
-module.exports = router;
